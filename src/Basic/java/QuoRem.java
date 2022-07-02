9b4e00cf3ee5b7b7cf256bf2b143787da17684e8
mohita <sachdevamohita5@gmail.com>
2022-07-02 19:47:55 +0530

    compute quo and remi

---
+++ src/Basic/java/QuoRem.java
@@ -0,0 +1,14 @@
+package Basic.java;
+
+public class QuoRem {
+    public static void main(String[]args)
+    {
+        int a=7;
+        int b=2;
+        int quo=a/b;
+        int remi=a%b;
+        System.out.println("print quo" +quo);
+        System.out.println("print reminder" +remi);
+
+    }
+}
