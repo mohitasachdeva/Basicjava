223ac4f0745327500bea15c7362bad07db5b88d6
mohita <sachdevamohita5@gmail.com>
2022-07-02 20:38:05 +0530

    power of 2

---
+++ src/Basic/java/Power2.java
@@ -0,0 +1,12 @@
+package Basic.java;
+
+public class Power2 {
+    public static void main(String[]args)
+    {
+        int a=2;
+        int b=6;
+        double c=Math.pow(a,b);
+                        System.out.println("print power of two-->" +c);
+    }
+
+}
