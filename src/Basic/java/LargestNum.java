10fcf844d3f915379117d5fad034db0546d5b316
mohita <sachdevamohita5@gmail.com>
2022-07-02 19:01:18 +0530

    find largest number

---
+++ src/Basic/java/LargestNum.java
@@ -0,0 +1,21 @@
+package Basic.java;
+
+public class LargestNum {
+    public static void main(String[]args) {
+
+
+        int a = 6;
+        int b = 5;
+        int c = 3;
+        if (a>b && a>c){
+            System.out.println("num a is greater->" +a);
+        }
+        else if(b>c){
+            System.out.println("num b is greater->" +b);
+        }
+else{
+            System.out.println("num c is greater->" +c);
+        }
+
+    }
+}
