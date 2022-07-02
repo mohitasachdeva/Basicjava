a78345976f85baa297c8d84f39b6ec731e459383
mohita <sachdevamohita5@gmail.com>
2022-07-02 15:28:48 +0530

    Initial commit

---
+++ src/Basic/java/LeapYear.java
@@ -0,0 +1,17 @@
+package Basic.java;
+
+public class LeapYear {
+    public static void main(String[]args)
+    {
+        int year= 2017;
+        if((year%400==0)||(year%4==0 &&year%100!==0));
+        {
+            System.out.println("leap year");
+        }
+        else
+        {
+            System.out.println("not a leap year");
+        }
+
+    }
+}
