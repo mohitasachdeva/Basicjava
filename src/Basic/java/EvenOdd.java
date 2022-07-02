b35a38e67db659651cf6421a70a21bbaf15ccfe9
mohita <sachdevamohita5@gmail.com>
2022-07-02 18:36:50 +0530

    check number is even or odd

---
+++ src/Basic/java/EvenOdd.java
@@ -0,0 +1,14 @@
+package Basic.java;
+
+public class EvenOdd {
+    public static void main(String[]arg){
+        int num=3;
+        if(num%2==0)
+        {
+            System.out.println("number is even->" +num);
+        }
+        else{
+            System.out.println("number is odd");
+        }
+    }
+}
